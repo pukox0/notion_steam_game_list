6108b63a53faa5e1626e101213b701197facd875
pukox0 <47234945+pukox0@users.noreply.github.com>
2026-03-30 21:45:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -159,7 +159,7 @@ def add_item_to_notion_database(game, achievements_info, review_text, steam_stor
                     }
                 ],
             },
-            "tags": {
+            "Tags": {
                 "type": "multi_select",
                 "multi_select": steam_store_data['tag']
             }
@@ -230,23 +230,23 @@ def update_item_to_notion_database(page_id, game, achievements_info, review_text
 
     data = {
         "properties": {
-            "name": {
+            "Name": {
                 "type": "title",
                 "title": [{"type": "text", "text": {"content": f"{game['name']}"}}],
             },
-            "playtime": {"type": "number", "number": playtime},
-            "last play": {"type": "date", "date": {"start": last_played_time}},
-            "store url": {
+            "Playtime": {"type": "number", "number": playtime},
+            "Last Play": {"type": "date", "date": {"start": last_played_time}},
+            "Store URL": {
                 "type": "url",
                 "url": store_url,
             },
-            "completion": {"type": "number", "number": completion},
-            "total achievements": {"type": "number", "number": total_achievements},
-            "achieved achievements": {
+            "Completion": {"type": "number", "number": completion},
+            "Total Achievements": {"type": "number", "number": total_achievements},
+            "Achieved Achievements": {
                 "type": "number",
                 "number": achieved_achievements,
             },
-            "review": {
+            "Review": {
                 "type": "rich_text",
                 "rich_text": [
                     {
@@ -255,7 +255,7 @@ def update_item_to_notion_database(page_id, game, achievements_info, review_text
                     }
                 ],
             },
-            "info": {
+            "Info": {
                 "type": "rich_text",
                 "rich_text": [
                     {
@@ -264,7 +264,7 @@ def update_item_to_notion_database(page_id, game, achievements_info, review_text
                     }
                 ],
             },
-            "tags": {
+            "Tags": {
                 "type": "multi_select",
                 "multi_select": steam_store_data['tag']
             }
